c5db5fab2255f98cf4fce996398143d75522e53a
Lewis Kabui <lewis.maina@andela.com>
2018-09-20 15:31:57 +0300

    Fix typo/wrong import path

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "react";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
